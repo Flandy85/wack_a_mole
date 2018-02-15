5834fdad6e9f48ab45ab3642d7f7ce681384ba94
Anders Gustavsson <anders@kreditz.com>
2018-02-15 09:36:52 +0100

    Added 2 more instance variables for finding the center of mole image in class WhackaMole in mole.rb

--- mole.rb
+++ mole.rb
@@ -12,9 +12,19 @@ class WhackaMole < Gosu::Window
 		# x and y instance variables that decides where mole image will appear in game window
 		@x = 200 # Distance from the left side of gamewindow (200px)
 		@y = 200 # Distance from top side of gamewindow (200px)
+
+		# @width and @height instance variables is for finding the center of the mole image
+		@width = 100
+		@height = 75
+	end
+
+	# Draws the mole image in gamewindow
+	def draw
+		@image.draw(@x - @width / 2, @y - @height / 2, 1) # image coords and if there are more images than 1, the number 1 decides position in gamewindow
 	end
 end
-# Save class in to variable
+
+# Save class in to variable window
 window = WhackaMole.new
 # Show gamewindow
 window.show
