90e77a43835036acf90c1eaa2ea351891f5a7337
Anders Gustavsson <anders@kreditz.com>
2018-02-15 16:22:03 +0100

    Added unless loop for showing results after timelimit with if statement presenting diffrent outcommes in mehtod draw in mole.rb

--- mole.rb
+++ mole.rb
@@ -18,8 +18,8 @@ class WhackaMole < Gosu::Window
 		@height = 75
 
 		# Instance variabels for movements of mole
-		@velocity_x = 4 # speed 4
-		@velocity_y = 4
+		@velocity_x = 5 # speed 5
+		@velocity_y = 5
 
 		# Instance variable for making mole blink
 		@visible = 0
@@ -31,8 +31,12 @@ class WhackaMole < Gosu::Window
 		@hit = 0
 
 		# Instance variable for keeping score and showing on screen
-		@font = Gosu::Font.new(30)
+		@font = Gosu::Font.new(20)
 		@score = 0
+		
+
+		# When time limit hits 0
+		@playing = true
 	end
 
 	# Draws the mole image in gamewindow
@@ -59,41 +63,72 @@ class WhackaMole < Gosu::Window
 		@hit = 0
 
 		# Show score on gamewindow
-		@font.draw("Your Score: #{ @score.to_s }", 500, 50, 2)	
+		@font.draw("Score: #{ @score.to_s }", 630, 50, 2)
+		@font.draw(@time_left.to_s, 30, 50, 2)
+
+		# Unless loop, activates when timelimit has reached zero.
+		unless @playing
+			if @score <= 0
+				@font.draw("Game over!! Your score: #{ @score }! Are you serious!?", 300, 300, 3)
+			end
+			if @score > 0 && @score <= 30
+				@font.draw("Game over!! Your score: #{ @score }!", 300, 300, 3)
+			end
+			if @score >= 31 && @score <= 60
+				@font.draw("Game over!! Your score: #{ @score }! Well done!", 250, 300, 3)
+			end
+			if @score >= 61 && @score < 90
+				@font.draw("Game over!! Your score: #{ @score }! SUPERPLAYER!", 250, 300, 3)
+			end
+			if @score > 90
+				@font.draw("Game over!! Your score: #{ @score }! GODLIKE!", 250, 300, 3)
+			end
+
+			@visible = 20
+			@time_left = 0
+		end	
 			
 	end
 
 	# Updates all animation and movements in the game
 	def update
-		@x += @velocity_x
-		@y += @velocity_y
+		# if playing the game
+		if @playing
+			@x += @velocity_x
+			@y += @velocity_y
 
-		# If mole hits game wall change direction instead of passing throu
-		@velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0 
-		@velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 1 
+			# If mole hits game wall change direction instead of passing throu
+			@velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0 
+			@velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 1 
 
-		# Making mole invisible for 10 frames
-		@visible -= 1
-		# Making mole visible for 50 frames
-		@visible = 50 if @visible < -10 && rand < 0.01
+			# Making mole invisible for 10 frames
+			@visible -= 1
+			# Making mole visible for 50 frames
+			@visible = 50 if @visible < -10 && rand < 0.01
 
-		# Time limit for game session, 1 minute
-		@time_left = (60 - (Gosu.milliseconds / 1000))
+			# Time limit for game session, 1 minute
+			@time_left = (60 - (Gosu.milliseconds / 1000))
+
+			# If timelimit has reached 0
+			@playing = false if @time_left < 0
+		end
 	end
 
 	# Method for handling mouse click, button_down is a gosu method that handles whatever
 	# if its a mouse, gamepad, keyboard or mousepad
 	def button_down(id)
-		# Using left mouse button, check if left mouse button was pressed
-		if(id == Gosu::MsLeft)
-			# Check if left mouse button was clicked with hammer distance 60 frames from mole image and if 
-			# mole image was visible
-			if Gosu.distance(mouse_x, mouse_y, @x, @y) < 60 && @visible >= 0
-				@hit = 1
-				@score += 10
-			else
-				@hit = -1
-				@score -= 3
+		if @playing
+			# Using left mouse button, check if left mouse button was pressed
+			if(id == Gosu::MsLeft)
+				# Check if left mouse button was clicked with hammer distance 60 frames from mole image and if 
+				# mole image was visible
+				if Gosu.distance(mouse_x, mouse_y, @x, @y) < 60 && @visible >= 0
+					@hit = 1
+					@score += 10
+				else
+					@hit = -1
+					@score -= 3
+				end
 			end
 		end
 	end
