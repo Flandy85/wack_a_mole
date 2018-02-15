3c42ab8a280a6892477244d1f6329c12bf1d13d7
Anders Gustavsson <anders@kreditz.com>
2018-02-15 09:24:08 +0100

    Added require gosu lib and created class WhackaMole in mole.rb

--- mole.rb
+++ mole.rb
@@ -1,1 +1,14 @@
 require 'gosu'
+
+# Class WackaMole
+class WhackaMole < Gosu::Window
+	def initialize
+		super(800,600)
+		self.caption = "Whack a Mole"
+
+	end
+end
+
+window = WhackaMole.new
+
+window.show
