edbe0de2ca27c3734c17c401aa78abb6f37833de
Anders Gustavsson <anders@kreditz.com>
2018-02-15 09:30:48 +0100

    Added instance variables and image instance variable in class WhackaMole in mole.rb and comments

--- mole.rb
+++ mole.rb
@@ -3,12 +3,18 @@ require 'gosu'
 # Class WackaMole
 class WhackaMole < Gosu::Window
 	def initialize
-		super(800,600)
-		self.caption = "Whack a Mole"
+		super(800,600) # Size of game window
+		self.caption = "Whack a Mole" # Game title on game window
 
+		# Instance variable that creates an instance of image
+		@image = Gosu::Image.new('images/mole.png')
+
+		# x and y instance variables that decides where mole image will appear in game window
+		@x = 200 # Distance from the left side of gamewindow (200px)
+		@y = 200 # Distance from top side of gamewindow (200px)
 	end
 end
-
+# Save class in to variable
 window = WhackaMole.new
-
+# Show gamewindow
 window.show
